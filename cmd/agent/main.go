de9934ed28550d41a3ba6a65153515ca600a5c8c
Todd <anatolev.web@icloud.com>
2024-11-10 08:15:30 +1000

    reportInterval < pollInterval

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,74 +1,95 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "reflect"
-    "strings"
-    "time"
+	"fmt"
+	"log"
+	"reflect"
+	"slices"
+	"strings"
+	"time"
 
-    . "github.com/anatolev-max/metrics-tpl/config"
-    "github.com/anatolev-max/metrics-tpl/internal/enum"
-    "github.com/anatolev-max/metrics-tpl/internal/storage"
+	. "github.com/anatolev-max/metrics-tpl/config"
+	"github.com/anatolev-max/metrics-tpl/internal/enum"
+	"github.com/anatolev-max/metrics-tpl/internal/storage"
 
-    "github.com/go-resty/resty/v2"
+	"github.com/go-resty/resty/v2"
 )
 
 func main() {
-    c := NewConfig()
-    parseFlags(c)
+	c := NewConfig()
+	parseFlags(c)
 
-    if err := run(); err != nil {
-        panic(err)
-    }
+	if err := run(); err != nil {
+		panic(err)
+	}
 }
 
 func run() error {
-    s := storage.NewMemStorage()
-    fmt.Println("Running agent\nServer endpoint", options.flagRunAddr)
-    diff := options.reportInterval % options.pollInterval
+	s := storage.NewMemStorage()
+	fmt.Println("Running agent\nServer endpoint", options.flagRunAddr)
+	intervals := []uint{options.pollInterval, options.reportInterval}
+	maxInterval := slices.Max(intervals)
+	minInterval := slices.Min(intervals)
+	diff := maxInterval % minInterval
 
-    var i uint = 0
-    for {
-        i++
-        time.Sleep(time.Duration(options.pollInterval) * time.Second)
-        //fmt.Println(i)
-        s.UpdateAgentData()
+	var i uint = 0
+	for {
+		i++
+		time.Sleep(time.Duration(minInterval) * time.Second)
+		//if options.pollInterval < options.reportInterval {
+		//	fmt.Println(i)
+		//} else {
+		//	fmt.Println("send")
+		//}
+		reportOrPoll(options.pollInterval > options.reportInterval, s)
 
-        if i%(options.reportInterval/options.pollInterval) == 0 {
-            if diff != 0 {
-                time.Sleep(time.Duration(diff) * time.Second)
-            }
-            //fmt.Println("send")
-            reportData(s)
-            i = 0
-        }
-    }
+		if i%(maxInterval/minInterval) == 0 {
+			if diff != 0 {
+				time.Sleep(time.Duration(diff) * time.Second)
+			}
+
+			//if options.pollInterval < options.reportInterval {
+			//	fmt.Println("send")
+			//	//i = 0
+			//} else {
+			//	fmt.Println(i)
+			//}
+			reportOrPoll(options.pollInterval < options.reportInterval, s)
+		}
+	}
+}
+
+func reportOrPoll(report bool, s storage.MemStorage) {
+	if report {
+		reportData(s)
+	} else {
+		s.UpdateAgentData()
+	}
 }
 
 func reportData(s storage.MemStorage) {
-    sValue := reflect.ValueOf(s)
+	sValue := reflect.ValueOf(s)
 
-    for fieldIndex := 0; fieldIndex < sValue.NumField(); fieldIndex++ {
-        metricType := sValue.Type().Field(fieldIndex).Name
+	for fieldIndex := 0; fieldIndex < sValue.NumField(); fieldIndex++ {
+		metricType := sValue.Type().Field(fieldIndex).Name
 
-        iter := sValue.FieldByName(metricType).MapRange()
-        for iter.Next() {
-            metricType = strings.ToLower(metricType)
-            url := fmt.Sprintf(options.flagRunAddr+enum.UpdateEndpoint+"/%v/%v/%v", metricType, iter.Key(), iter.Value())
-            sendRequest(url)
-        }
-    }
+		iter := sValue.FieldByName(metricType).MapRange()
+		for iter.Next() {
+			metricType = strings.ToLower(metricType)
+			url := fmt.Sprintf(options.flagRunAddr+enum.UpdateEndpoint+"/%v/%v/%v", metricType, iter.Key(), iter.Value())
+			sendRequest(url)
+		}
+	}
 }
 
 func sendRequest(url string) {
-    client := resty.New()
+	client := resty.New()
 
-    _, err := client.R().
-        SetHeader("Content-Type", enum.TextPlain).
-        Post(url)
+	_, err := client.R().
+		SetHeader("Content-Type", enum.TextPlain).
+		Post(url)
 
-    if err != nil {
-        log.Fatalln(err)
-    }
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
